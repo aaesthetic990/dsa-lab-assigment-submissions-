09a8f47c95a86ff0024b96b766bacf200671def1
aaesthetic990 <aaastha_be24@thapar.edu>
2025-08-11 16:45:19 +0530

    added question2.c

--- question2.c
+++ question2.c
@@ -1,1 +1,36 @@
 
+#include <stdio.h>
+
+int main() {
+    int number ;
+    printf("Enter number of elements: ");
+    scanf("%d", &number );
+
+    int arr[number];
+    printf("Enter %d elements: ", number );
+    for (int i = 0; i < number; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    for (int i = 0; i < number ; i++) {
+        for (int j = i + 1; j < number; ) {
+            if (arr[i] == arr[j]) {
+                
+                for (int k = j; k < number - 1; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                number--; 
+            } else {
+                j++;
+            }
+        }
+    }
+
+    printf("Array after removing duplicates: ");
+    for (i = 0; i < number; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
