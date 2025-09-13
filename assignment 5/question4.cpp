0ad5c69fba3a5e3edd6b719a9b7551744c2e0155
aaesthetic990 <aaastha_be24@thapar.edu>
2025-09-13 19:50:26 +0530

    Create question4.cpp

---
+++ assignment 5/question4.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* head = NULL;
+
+void insertAtEnd(int x) {
+    Node* newNode = new Node();
+    newNode->data = x;
+    newNode->next = NULL;
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next != NULL) temp = temp->next;
+    temp->next = newNode;
+}
+
+void reverseList() {
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* next = NULL;
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+}
+
+void display() {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    insertAtEnd(1);
+    insertAtEnd(2);
+    insertAtEnd(3);
+    insertAtEnd(4);
+
+    cout << "Original List: ";
+    display();
+
+    reverseList();
+
+    cout << "Reversed List: ";
+    display();
+
+    return 0;
+}
